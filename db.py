738ca7cb57278177176b0f9aca7d4010f24091ef
Supratim halder <supratimhldr6@gmail.com>
2020-10-08 21:59:45 +0530

    Delete db.py
    Inside dir

--- db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import config as cf
-
-engine = create_engine(cf.DATABASE_URI)
-Session = sessionmaker(bind=engine)
-session = Session()
